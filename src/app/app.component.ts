d3bc0cd0b3e41eb7b67830bcb19e818db025e63a
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-01-30 13:14:33 +0100

    added basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeroComponent, AboutComponent],
+  imports: [CommonModule, RouterOutlet, HeroComponent, AboutComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
