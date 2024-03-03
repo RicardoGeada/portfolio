09e29feb76f95af2d218a13fe2188f27cda14282
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-03-04 00:46:46 +0100

    added burger menu button

--- src/app/hero/navbar/navbar.component.ts
+++ src/app/hero/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  active: boolean = false;
 }
