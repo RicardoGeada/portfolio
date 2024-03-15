f3520d78c7d83d3bc470dc432d8591833cb8f036
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-03-15 14:16:29 +0100

    fixed navbar overlay hidden problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { HeaderComponent } from './shared/components/header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MainContentComponent],
+  imports: [CommonModule, RouterOutlet, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroComponent, AboutComponent, SkillsComponent, PortfolioComponent, ContactComponent],
+  imports: [HeroComponent, AboutComponent, SkillsComponent, PortfolioComponent, ContactComponent, HeaderComponent, FooterComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
