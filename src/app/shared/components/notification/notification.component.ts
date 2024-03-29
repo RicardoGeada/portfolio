4b8a9b2b2907e2a768d9401f65b166b223797a4b
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-03-29 11:40:25 +0100

    added linkedin link, upadate imprint

---
+++ src/app/shared/components/notification/notification.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  standalone: true,
+  imports: [],
+  templateUrl: './notification.component.html',
+  styleUrl: './notification.component.scss'
+})
+export class NotificationComponent {
+
+  @Input("message") message : string = '';
+
+}
