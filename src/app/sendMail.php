bf83e19611b3edf6ff5280868d598061aca1e0c7
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-06-28 00:33:05 +0200

    quick fix email

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'mail@ricardoggeada.com';  
+            $recipient = 'mail@ricardogeada.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
